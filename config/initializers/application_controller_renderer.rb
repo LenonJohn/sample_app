65c3c34b5e27a8e6cfb66f18e4b7af61788c952a
EC2 Default User <ec2-user@ip-172-31-61-25.ec2.internal>
2021-04-08 05:20:30 +0000

    [add]画像投稿　編集　削除

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ec56d29a6d832a0e848c726cf313326294a24b20afb1f46b61c643690e0dd511f2672f71dc0bfee5ad9c6b17a6eed350b739cac95178d761e542aca67cefe91c'
